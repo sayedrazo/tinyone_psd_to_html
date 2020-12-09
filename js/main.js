d73745caf43ef640ecff9bd63eea909beb2dedc0
Sayed Razo <63364395+sayedrazo@users.noreply.github.com>
2020-12-09 22:06:01 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,78 @@
+(function($){
+    "use strict";
+
+     $(document).ready(function($){
+        $('.owl-carousel').owl-carousel({
+            autoplay: true,
+            center: true,
+            loop: true,
+            nav: true,
+            items:3,
+            mouseDrag:true,
+            touchDrag:true,
+        });
+        
+    //     new WOW().init();
+
+    //     $(".owl-carousel").on("translate.owl.carousel", function(){
+    //        $(".single-slide-item h2,.single-slide-item p").removeClass("animated fadeInUp").css("opacity","1");
+    //        $(".single-slide-item .factorian-btn").removeClass("animated pulse").css("opacity","1");
+    //     });
+    //     $(".owl-carousel").on("translated.owl.carousel", function(){
+    //       $(".single-slide-item h2, .single-slide-item p").addClass("animated fadeInUp").css("opacity","1");
+    //       $(".single-slide-item .factorian-btn").addClass("animated pulse").css("opacity","1");
+    //     });
+    //      $(".owl-carousel").on("translate.owl.carousel", function () {
+    //          $(".single-slide-item-2 h2,.single-slide-item p").removeClass("animated fadeInUp").css("opacity", "1");
+    //          $(".single-slide-item-2 .factorian-btn").removeClass("animated pulse").css("opacity", "1");
+    //      });
+    //      $(".owl-carousel").on("translated.owl.carousel", function () {
+    //          $(".single-slide-item-2 h2, .single-slide-item-2 p").addClass("animated fadeInUp").css("opacity", "1");
+    //          $(".single-slide-item-2 .factorian-btn").addClass("animated pulse").css("opacity", "1");
+    //      });
+
+    //     $("ul#navigation").slicknav({
+    //         prependeTo: ".responsive-menu-wrep"
+    //     });
+        
+
+    //     $('.gallery-lightbox').magnificPopup({
+    //         type:'image',
+    //         preloader: true,
+    //         gallery:{
+    //             enabled:true,
+    //         }
+
+    //     });
+        
+
+
+       
+    // });
+
+    //     // slicknav
+    //     $(".main-menu").slicknav({
+    //         'prependTo' : ".responsive-menu-wrep"
+    //     });
+   
+    
+
+    //     jQuery(window).on("load",function(){
+    //         jQuery(".factorian-slide-preloder-wrapper").fadeOut(1000);
+    //     });
+
+
+    // // jQuery(window).load(function(){
+    // //     $(".factorian-slide-preloder-wrapper").fadeOut(1000);
+
+        
+        
+  });
+   
+         
+            
+
+}(jQuery));
+
+
+
